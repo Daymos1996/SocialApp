1c16f6889b459438fbd82d69d93173a98cabd0c3
Daymos1996 <38887719+Daymos1996@users.noreply.github.com>
2019-01-16 23:17:11 +0100

    W koncu udało sie przeslac na gita po wielu problemach

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,18 @@
 package daymos.lodz.uni.math.pl.socialapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,15 +22,25 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.model.ShareVideo;
+import com.facebook.share.model.ShareVideoContent;
+import com.facebook.share.widget.ShareDialog;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -33,15 +51,18 @@ public class MainActivity extends AppCompatActivity {
     private CallbackManager mCallbackManager;
 
     private static final String TAG = "FACELOG";
-    private static Activity activity;
     private ImageView avatar;
     private TextView FirstName;
     private TextView LastName;
     private TextView email;
-    private TextView gender;
-    private TextView birthday;
-    private TextView friends;
+    private Button shareLink;
+    private Button sharePhoto;
+    private Button shareVideo;
     private String Accesstoken;
+    public static final int PICK_IMAGE = 1;
+    public static final int PICK_VIDEO = 2;
+    private ProgressDialog mProgresDiaolog;
+    ShareDialog shareDialog;
 
 
 
@@ -52,26 +73,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("SocialApp");
-        avatar = findViewById(R.id.imageView);
-        FirstName = findViewById(R.id.txtFirstName);
-        LastName = findViewById(R.id.txtLastName);
-        email = findViewById(R.id.txtEmail);
-        gender = findViewById(R.id.txtGender);
-        friends = findViewById(R.id.txtFriends);
-        birthday = findViewById(R.id.txtBirthday);
-
-
-
-
-
-
-
-
-
+        init();
 
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.login_button);
+        shareDialog = new ShareDialog(this);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        mProgresDiaolog = new ProgressDialog(this);
+
+
+
+        final LoginButton loginButton = findViewById(R.id.login_button);
        // loginButton.setReadPermissions("email", "public_profile","user_birthday","user_friends");
         loginButton.setReadPermissions("email", "public_profile");
 
@@ -90,21 +101,34 @@ public class MainActivity extends AppCompatActivity {
 
                                 // Getting FB User Data
                                 getFacebookData(jsonObject);
+                                shareLink.setVisibility(View.VISIBLE);
+                                sharePhoto.setVisibility(View.VISIBLE);
+                                shareVideo.setVisibility(View.VISIBLE);
+
                             }
                         });
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,first_name,last_name,email,gender,birthday,friends");
+                parameters.putString("fields", "id,first_name,last_name,email");
                 request.setParameters(parameters);
                 request.executeAsync();
 
-                Toast.makeText(MainActivity.this, "Udalo sie zalogowac ", Toast.LENGTH_LONG).show();
 
+
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        LoginManager.getInstance().logOut();
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
 
             }
 
             @Override
             public void onCancel() {
                 Log.d(TAG, "facebook:onCancel");
+
             }
 
             @Override
@@ -114,17 +138,86 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        if(AccessToken.getCurrentAccessToken() != null){
-            email.setText(AccessToken.getCurrentAccessToken().getUserId());
-        }
-    }
+        shareLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setQuote("this is useful link")
+                        .setContentUrl(Uri.parse("https://youtube.com"))
+                        .build();
+                if(ShareDialog.canShow(ShareLinkContent.class)){
+                    shareDialog.show(linkContent);
+                }
+            }
+        });
+
+        sharePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPhotoChooser();
+
+            }
+        });
+
+
+
+        shareVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openVideoChooser();
+            }
+        });
+}
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Pass the activity result back to the Facebook SDK
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+
+
+            if(requestCode == PICK_VIDEO ){
+            Uri selectedVideo = data.getData();
+
+                ShareVideo video = new ShareVideo.Builder()
+                        .setLocalUrl(selectedVideo)
+                        .build();
+
+                ShareVideoContent videoContent = new ShareVideoContent.Builder()
+                        .setContentTitle("Moje film")
+                        .setContentDescription("moje ostatnie wspomnienia")
+                        .setVideo(video)
+                        .build();
+
+                if(shareDialog.canShow(ShareVideoContent.class))
+                    shareDialog.show(videoContent);
+            }
+
+        if (requestCode == PICK_IMAGE) {
+            Uri mImageProfileUri = data.getData();
+            try {
+                Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageProfileUri);
+
+                Toast.makeText(this, image+"dziala", Toast.LENGTH_SHORT).show();
+
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setBitmap(image)
+                        .build();
+
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(photo)
+                        .build();
+
+                if(ShareDialog.canShow(SharePhotoContent.class)){
+                    shareDialog.show(content);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
@@ -158,15 +251,7 @@ public class MainActivity extends AppCompatActivity {
             if (object.has("email"))
                 bundle.putString("email", object.getString("email"));
                 email.setText(object.getString("email"));
-            if (object.has("gender"))
-                bundle.putString("gender", object.getString("gender"));
-                gender.setText(object.getString("gender"));
-            if (object.has("birthday"))
-                bundle.putString("birthday", object.getString("birthday"));
-                birthday.setText(object.getString("birthday"));
-            if (object.has("friends"))
-                bundle.putString("friends", object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
-                friends.setText("Liczba znajomych "+ object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
+
 
 
 
@@ -177,5 +262,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void openPhotoChooser() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        // intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+    private void openVideoChooser() {
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("video/*");
+        startActivityForResult(intent, PICK_VIDEO);
 
+    }
+
+
+
+
+        private void init() {
+        setTitle("SocialApp");
+        avatar = findViewById(R.id.imageView);
+        FirstName = findViewById(R.id.txtFirstName);
+        LastName = findViewById(R.id.txtLastName);
+        email = findViewById(R.id.txtEmail);
+        shareLink = (Button) findViewById(R.id.buttonShareLink);
+        sharePhoto = (Button) findViewById(R.id.buttonSharePhoto);
+        shareVideo = (Button) findViewById(R.id.buttonShareVideo);
+
+    }
 }
